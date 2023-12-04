17e9c47eb4c2d512920fa7a09f9e3ce280107f64
Omar Faruk <mohon.sust@gmail.com>
2023-12-04 17:01:32 +0600

    Final MR Update Function

--- InsuranceManagementAPI/Data/Repository/IFinalMRRepository.cs
+++ InsuranceManagementAPI/Data/Repository/IFinalMRRepository.cs
@@ -5,6 +5,7 @@ namespace InsuranceManagementAPI.Data.Repository
     public interface IFinalMRRepository
     {
         Task<long> Add(FinalMRDto finalMRDto);
+        Task<bool> Update(FinalMRDto finalMRDto);
         Task<FinalMRDto> GetFinalMRByID(long FinalMRKey);
     }
 }
