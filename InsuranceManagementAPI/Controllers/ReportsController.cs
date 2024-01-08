b60bd6b67bb8be1c7bb44220e876ec6ebcdf2dc2
Ruhul <ruhul.ewu00@gmail.com>
2024-01-08 13:14:58 +0600

    Added missing ReportsController

---
+++ InsuranceManagementAPI/Controllers/ReportsController.cs
@@ -0,0 +1,83 @@
+﻿using AspNetCore.Reporting;
+using AspNetCore.Reporting.ReportExecutionService;
+using InsuranceManagementAPI.Extensions;
+using InsuranceManagementAPI.Models;
+using InsuranceManagementAPI.Models.Report;
+using InsuranceManagementAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net.Mime;
+
+
+namespace InsuranceManagementAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/Bank")]
+    [ApiVersion("1.0")]
+    public class ReportsController : ControllerBase
+    {
+        private readonly IReportingService _reportingService;
+
+        public ReportsController(IReportingService reportingService)
+        {
+            _reportingService = reportingService;
+        }
+
+        [MapToApiVersion("1.0")]
+        [HttpPost("GetAllBanks")]
+        public ActionResult GetAllBanks(BankReportParam param)
+        {
+            ReportDocument file;
+
+            try
+            {
+                file = _reportingService.GenerateBankReport(param);
+
+                if (file.FilePath.IsNullOrEmpty())
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            return Ok(file);
+        }
+
+        [MapToApiVersion("1.0")]
+        [HttpPost("ReportFinalMR")]
+        public ActionResult GenerateReportFinalMR(FinalMRReporParam param)
+        {
+            ReportDocument file;
+
+            try
+            {
+                file = _reportingService.ReportFinalMR(param);
+
+                if (file.FilePath.IsNullOrEmpty())
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            return Ok(file);
+        }
+
+        [MapToApiVersion("1.0")]
+        [HttpPost("FinalMRReport")]
+        public ActionResult GenerateFinalMRReport(FinalMRReporParam param)
+        {
+            ReportDocument file = _reportingService.ReportFinalMR(param);
+
+            return File(file.FileStream, MediaTypeNames.Application.Octet, file.FileName);
+        }
+    }
+}
